4af27cdb4c7166159b2cf752d504e7b3d09a3088
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-18 10:24:41 +0300

    комментария и TODO

--- Scripts/YouDiedScript.cs
+++ Scripts/YouDiedScript.cs
@@ -5,12 +5,13 @@ using UnityEngine.Experimental.UIElements;
 
 public class YouDiedScript : MonoBehaviour {
 
-    public  GameObject LButton;
-    public  GameObject RButton;
+    public  GameObject LButton; // кнопка вернуться в главное меню
+    public  GameObject RButton; // Кнопка загрузить последнее сохранение
 
     public void SetActiveAllButtons()
     {
         LButton.SetActive(true);
+        //TODO: Здесь нужна проверка на то, что если нет последнего сохранения, то или не отображать кнопку или делать ее темной
         RButton.SetActive(true);
     }
 }
