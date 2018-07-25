aea5060a38b1a2bbdfb515dac15e70ce9f0ef94a
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-25 11:22:26 +0300

    Добавил все KeyCode для всех кнопок.

--- Scripts/SettingsData.cs
+++ Scripts/SettingsData.cs
@@ -24,8 +24,6 @@ public class SettingsData {
     //Поля с панели Управление
     public Dictionary<string, KeyCode> keys;
 
-    //public KeyCode KeyUp;
-
     //Класс для сохранения
     [System.Serializable]
     public class PlayerSettings //Класс для сохранения данных персонажа
@@ -44,6 +42,20 @@ public class SettingsData {
         public float music;
 
         public string KeyUp;
+        public string KeyLeft;
+        public string KeyDown;
+        public string KeyRight;
+        public string KeyRoll;
+        public string KeyAcceleration;
+        public string KeyInteraction;
+        public string KeyInventory;
+        public string KeyMap;
+        public string KeyJournal;
+        public string KeySkills;
+        public string KeyOrganizer;
+        public string KeyMenu;
+        public string KeyLpocket;
+        public string KeyRpocket;
     }
 
 
@@ -72,7 +84,20 @@ public class SettingsData {
                 music = positionPlayer.music;
 
                 keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyUp));
-                //KeyUp = (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyUp); 
+                keys.Add("Left",(KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyLeft));
+                keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyDown));
+                keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyRight));
+                keys.Add("Roll", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyRoll));
+                keys.Add("Acceleration", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyAcceleration));
+                keys.Add("Interaction", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyInteraction));
+                keys.Add("Inventory", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyInventory));
+                keys.Add("Map", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyMap));
+                keys.Add("Journal", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyJournal));
+                keys.Add("Skills", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeySkills));
+                keys.Add("Organizer", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyOrganizer));
+                keys.Add("Menu", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyMenu));
+                keys.Add("Lpocket", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyLpocket));
+                keys.Add("Rpocket", (KeyCode)System.Enum.Parse(typeof(KeyCode), positionPlayer.KeyRpocket));
             }
             catch (System.Exception e)
             {
@@ -99,7 +124,20 @@ public class SettingsData {
             music = 0.5f;
 
             keys.Add("Up", KeyCode.W);
-            //KeyUp = KeyCode.W;
+            keys.Add("Left", KeyCode.A);
+            keys.Add("Down", KeyCode.S);
+            keys.Add("Right", KeyCode.D);
+            keys.Add("Roll", KeyCode.Space);
+            keys.Add("Acceleration", KeyCode.LeftShift);
+            keys.Add("Interaction", KeyCode.E);
+            keys.Add("Inventory", KeyCode.I);
+            keys.Add("Map", KeyCode.M);
+            keys.Add("Journal", KeyCode.J);
+            keys.Add("Skills", KeyCode.K);
+            keys.Add("Organizer", KeyCode.Tab);
+            keys.Add("Menu", KeyCode.Esc);
+            keys.Add("Lpocket", KeyCode.R);
+            keys.Add("Rpocket", KeyCode.F);
 
             SaveSettings();
         }
@@ -123,6 +161,20 @@ public class SettingsData {
         positionPlayer.music = music;
 
         positionPlayer.KeyUp = keys["Up"].ToString();
+        positionPlayer.KeyLeft = keys["Left"].ToString();
+        positionPlayer.KeyDown = keys["Down"].ToString();
+        positionPlayer.KeyRight = keys["Right"].ToString();
+        positionPlayer.KeyRoll = keys["Roll"].ToString();
+        positionPlayer.KeyAcceleration = keys["Acceleration"].ToString();
+        positionPlayer.KeyInteraction = keys["Interaction"].ToString();
+        positionPlayer.KeyInventory = keys["Inventory"].ToString();
+        positionPlayer.KeyMap = keys["Map"].ToString();
+        positionPlayer.KeyJournal = keys["Journal"].ToString();
+        positionPlayer.KeySkills = keys["Skills"].ToString();
+        positionPlayer.KeyOrganizer = keys["Organizer"].ToString();
+        positionPlayer.KeyMenu = keys["Menu"].ToString();
+        positionPlayer.KeyLpocket = keys["Lpocket"].ToString();
+        positionPlayer.KeyRpocket = keys["Rpocket"].ToString();
 
         //Создаем новый файл (или перезаписываем старый)
         FileStream fs = new FileStream(Application.dataPath + "/settings.set", FileMode.Create);
